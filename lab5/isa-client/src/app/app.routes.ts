2c13b108d282cd22c17bb6fd8cb1f9fe20f22e52
mvollkova <150148732+mvollkova@users.noreply.github.com>
2025-12-16 16:59:56 +0100

    delete

--- lab5/isa-client/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GenreListComponent } from './components/genre-list/genre-list.component'; 
-import { GenreAddComponent } from './components/genre-add/genre-add.component';
-export const routes: Routes = [
-  { path: 'genres', component: GenreListComponent },
-  { path: 'genres/add', component: GenreAddComponent },
-
-  { path: '', redirectTo: '/genres', pathMatch: 'full' },
-];
