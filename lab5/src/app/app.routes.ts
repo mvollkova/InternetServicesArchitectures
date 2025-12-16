8835dd66ea740e6f28e371e0a3259218ccbc4e3b
mvollkova <150148732+mvollkova@users.noreply.github.com>
2025-12-16 21:40:56 +0100

    delete

--- lab5/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-
-import { Routes } from '@angular/router';
-import { GenreListComponent } from './genres/genre-list/genre-list.component';
-
-export const routes: Routes = [
-  { path: 'genres', component: GenreListComponent },
-
-  { path: '', redirectTo: '/genres', pathMatch: 'full' },
-];
